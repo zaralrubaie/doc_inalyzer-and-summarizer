a883c060b8c1b7329065e77e2d4d6aebefee4593
zahraa <167252881+zaralrubaie@users.noreply.github.com>
2026-02-16 23:28:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import os
 
 # ====== CONFIGURE GEMINI ======
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
-model = genai.GenerativeModel("models/gemini-2.5-pro")
+model = genai.GenerativeModel("gemini-2.5-flash")
 #
 app = FastAPI()
 
